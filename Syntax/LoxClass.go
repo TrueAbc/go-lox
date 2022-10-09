29fed13ea278a408f99364140a40f2a31385997a
trueabc <547551933@qq.com>
2022-10-09 20:50:53 +0800

    12-2 对象实例化

--- Syntax/LoxClass.go
+++ Syntax/LoxClass.go
@@ -4,10 +4,27 @@ type LoxClass struct {
 	name string
 }
 
+func (lc *LoxClass) Arity() int {
+	return 0
+}
+
+func (lc *LoxClass) Call(interpreter *Interpreter, args []interface{}) interface{} {
+	instance := &LoxInstance{lc}
+	return instance
+}
+
 func (lc *LoxClass) String() string {
 	return lc.name
 }
 
 func NewLoxClass(name string) *LoxClass {
 	return &LoxClass{name: name}
+}
+
+type LoxInstance struct {
+	kClass *LoxClass
+}
+
+func (li *LoxInstance) String() string {
+	return li.kClass.String() + " instance."
 }
